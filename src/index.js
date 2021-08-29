345d46730c0f35de010e6f89a1eca0663bb72041
zlobnikov <daffodil@list.ru>
2021-08-29 14:08:02 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = {};
+  bracketsConfig.forEach(brackets => {
+    openBrackets[brackets[0]] = brackets[1];
+  });
+
+  const waitingList = [];
+
+  for (let i = 0; i < str.length; ++i) {
+    const bracket = str[i];
+
+    if (bracket === waitingList[waitingList.length - 1]) {
+      waitingList.pop();
+
+    } else if (openBrackets[bracket]) {
+      waitingList.push(openBrackets[bracket]);
+
+    } else return false;
+  }
+
+  return waitingList.length === 0;
 }
